b104dc3505ee52a7deb5b190b1e744489998a1b0
hh <songkh2102@163.com>
2024-12-19 22:00:38 +0800

    测试背景颜色

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page} >
-    <Image src="/ChaToken.png" width={369} height={80} alt="icon"/>
+      <Image src="/ChaToken.png" width={369} height={80} alt="icon" />
     </div>
   );
 }
