be355ef41e56cd619255de90e1464fd90a0e2193
hh <songkh2102@163.com>
2024-12-22 23:30:16 +0800

    添加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import GoogleAnalytics from "../../components/GoogleAnalytics";
+import { Suspense } from 'react'
+import Script from 'next/script'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,6 +28,14 @@ export default function RootLayout({
     <html lang="cn">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Suspense fallback={<div>Loading...</div>}>
+          <GoogleAnalytics />
+          {children}
+          <Script
+          src="https://widgets.coingecko.com/gecko-coin-list-widget.js"
+          strategy="afterInteractive"
+        />
+        </Suspense>
       </body>
     </html>
   );
